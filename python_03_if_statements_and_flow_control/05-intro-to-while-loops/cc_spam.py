d0ad624b9964d90646f82d21e5d38020c2507b01
sethmcknight <7377802+sethmcknight@users.noreply.github.com>
2025-05-06 22:44:24 -0600

    Add cc_spam.py script to remove "Spam" from a list of ingredients

---
+++ python_03_if_statements_and_flow_control/05-intro-to-while-loops/cc_spam.py
@@ -0,0 +1,6 @@
+ingredients = ["Spam", "sausage", "Spam", "Spam", "Spam", "bacon", "Spam", "tomato", "Spam", "Spam", "Spam", "Spam", "Spam", "Spam", "baked beans", "Spam", "Spam", "Spam", "Spam"]
+
+while "Spam" in ingredients:
+    ingredients.remove("Spam")
+
+print(f"Spamless ingredients: {ingredients}")

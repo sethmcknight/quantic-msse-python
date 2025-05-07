31cc377ae8e294257563200e1c6773bf3363dfd8
sethmcknight <7377802+sethmcknight@users.noreply.github.com>
2025-05-06 22:23:16 -0600

    Refactor pulse rate conversion to use None instead of "NA" for out-of-range values and improve type hinting

--- python_03_if_statements_and_flow_control/04-combining-if-statements-and-for-loops/pulse.py
+++ python_03_if_statements_and_flow_control/04-combining-if-statements-and-for-loops/pulse.py
@@ -1,11 +1,19 @@
+from typing import List, Optional
+
+# This code takes a list of pulse rates and converts them to a new scale.
+
+# example pulse rates
 rates = [17, 21, 180, 24]
 
-converted_rates = []
+# Initialize an empty list to store the converted rates using List[Optional[int]] to allow for None values
+converted_rates: List[Optional[int]] = []
 
+# Iterate through the pulse rates and convert them
+# to a new scale if they are within the specified range``
 for rate in rates:
     if rate >= 15 and rate <= 25:
         converted_rates.append(rate * 4)
     else:
-        converted_rates.append("NA")
+        converted_rates.append(None)
     
 print(converted_rates)
